b3b2f2098cf641d1a48d04e12f6e2f621097ff0d
SARTHAKKRSHARMA <f20190449@pilani.bits-pilani.ac.in>
2023-12-23 21:12:42 +0530

    cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-platform-nxtms5j9x-sarthakkrsharmas-projects.vercel.app/",
 		credentials:true,
 	})
 )
